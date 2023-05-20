92b6f01c9de1f1336d7a3c8f3371655badc2219b
mrdbrg <rdg.brg@gmail.com>
2023-05-20 13:34:06 -0500

    print out car bookings / print out electric cars

--- CLI/Main.java
+++ CLI/Main.java
@@ -1,5 +1,7 @@
 package com.havefunwith.CLI;
 
+import com.havefunwith.CLI.booking.CarBooking;
+import com.havefunwith.CLI.booking.CarBookingService;
 import com.havefunwith.CLI.car.Car;
 import com.havefunwith.CLI.car.CarService;
 import com.havefunwith.CLI.enums.Brand;
@@ -21,40 +23,58 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        CARS
+        // CARS
         Car tesla = new Car("1234", BigDecimal.valueOf(89.00), Brand.TESLA, true);
         Car audi = new Car("5678", BigDecimal.valueOf(50.00), Brand.AUDI, false);
         Car mercedes = new Car("91011", BigDecimal.valueOf(77.00), Brand.MERCEDES, false);
 
-//        USERS
+        // USERS
         User tom = new User("123", "Tom", 24);
         User betty = new User("456", "Betty", 28);
+        // USERS END
 
-        System.out.println("TESLA : " + tesla);
-        System.out.println("AUDI : " + audi);
-        System.out.println("MERCEDES : " + mercedes);
+        // CAR BOOKING
+        CarBooking tomBooking = new CarBooking(tesla, tom);
+        CarBooking bettyBooking = new CarBooking(audi, betty);
+        // CAR BOOKING END
 
-        // INSTANCES OF CLASSES
+        // CLASS INSTANCES
         CarService carService = new CarService();
         UserService userService = new UserService();
+        CarBookingService carBookingService = new CarBookingService();
+        // CLASS INSTANCES END
 
         // CREATE CARS
         carService.createCar(tesla);
+        // CREATE CARS END
 
         // CREATE USERS
         userService.createUser(tom);
         userService.createUser(betty);
+        // CREATE USERS END
+
+        // CREATE CAR BOOKING
+        carBookingService.createCarBooking(tomBooking);
+        carBookingService.createCarBooking(bettyBooking);
+        // CREATE CAR BOOKING END
 
         // DISPLAY
-        System.out.println();
-        System.out.println(Arrays.toString(
-                            Arrays.copyOf(carService.returnCars(),
-                                    carService.returnNumberOfTotalCars())));
+        System.out.println("\n");
+        System.out.println("============== Cars ==============");
+        System.out.println(Arrays.toString(carService.returnCars()));
+        System.out.print("ELECTRIC CARS: " + Arrays.toString(carService.returnElectricCars()));
 
+        System.out.println("\n");
+        System.out.println("============== Users ==============");
         System.out.println(Arrays.toString(
                             Arrays.copyOf(userService.returnUsers(),
                                     userService.returnNumberOfTotalUsers())));
 
+        System.out.println("\n");
+        System.out.println("============== Car Bookings ==============");
+        System.out.println(Arrays.toString(
+                            Arrays.copyOf(carBookingService.returnCarBookings(),
+                                        carBookingService.returnNumberOfTotalCarBookings())));
     }
 
 }
