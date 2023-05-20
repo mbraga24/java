bf347fd3a5028a181052e131bb1997a7042b03f4
mrdbrg <rdg.brg@gmail.com>
2023-05-20 13:34:40 -0500

    add method - returnElectricCars()

--- CLI/car/CarService.java
+++ CLI/car/CarService.java
@@ -1,5 +1,7 @@
 package com.havefunwith.CLI.car;
 
+import java.util.Arrays;
+
 public class CarService {
 
     private CarDAO carDao;
@@ -12,8 +14,30 @@ public class CarService {
         carDao.saveCar(car);
     }
 
+    /**
+     * Returns all cars.
+     * @return array of cars.
+     */
     public Car[] returnCars() {
-        return carDao.getAllCars();
+       return Arrays.copyOf(carDao.getAllCars(), this.returnNumberOfTotalCars());
+    }
+
+    /**
+     * Returns all electric cars - All cars with isElectric boolean set to true.
+     * @return array of electric cars.
+     */
+    public Car[] returnElectricCars() {
+        int length = carDao.getAllCars().length;
+        Car[] electricCars; // O(n)
+        electricCars = Arrays.copyOf(carDao.getAllCars(), this.returnNumberOfTotalCars()); // O(n)
+        int carCount = 0;
+        for (int i = 0; i < electricCars.length; i++) { // O(n)
+            if (electricCars[i].getIsElectric() == true) {
+                electricCars[i] = electricCars[i];
+                carCount++;
+            }
+        }
+        return Arrays.copyOf(electricCars, carCount); // O(n)
     }
 
     public int returnNumberOfTotalCars() {
