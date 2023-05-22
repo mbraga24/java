b53346bcda4904f784fb35a5966fe4dd14c15eee
mrdbrg <rdg.brg@gmail.com>
2023-05-22 07:57:39 -0500

    SOLID principles application

---
+++ modules/demos/solid/Circle.java
@@ -0,0 +1,15 @@
+package com.havefunwith.modules.demos.solid;
+
+public class Circle {
+
+    private final int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+}
