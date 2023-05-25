c62bf9d6d66bae3cc551ac8fe6550ab555218cee
mrdbrg <rdg.brg@gmail.com>
2023-05-25 09:03:36 -0500

    create display generic class

--- CLI/utils/Utils.java
+++ CLI/utils/Utils.java
@@ -2,6 +2,12 @@ package com.havefunwith.CLI.utils;
 
 public class Utils {
 
+    /**
+     * Method uses generics to display objects from any given array.
+     *
+     * @param array
+     * @param <T>
+     */
     public static <T> void display(T[] array) {
         for (T item : array) {
             System.out.println(item.toString());
