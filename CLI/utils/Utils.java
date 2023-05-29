5358c2b0874ac89a28b506ac27f938d57df930b9
mrdbrg <rdg.brg@gmail.com>
2023-05-29 14:17:05 -0500

    add printMessage() method

--- CLI/utils/Utils.java
+++ CLI/utils/Utils.java
@@ -4,7 +4,6 @@ public class Utils {
 
     /**
      * Method uses generics to display objects from any given array.
-     *
      * @param array
      * @param <T>
      */
@@ -12,6 +11,36 @@ public class Utils {
         for (T item : array) {
             System.out.println(item.toString());
         }
+    }
+
+    /**
+     * Helps build message and separator header to avoid repetition.
+     * @param message
+     */
+    public static void printMessage(String message) {
+        int countChars = 0;
+        char[] chars = message.toCharArray();
+        String[] messageArr = message.split(" ");
+
+        for (int i = 0; i < messageArr.length; i++) {
+            if (messageArr[i].equals("+")) {
+                System.out.print("");
+            }
+            else {
+                System.out.print(messageArr[i] + " ");
+            }
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            countChars++;
+            if (chars[i] == '+') break;
+        }
+
+        System.out.println();
+        for (int i = 0; i < countChars; i++) {
+            System.out.print("=");
+        }
+        System.out.println();
     }
 
 }
