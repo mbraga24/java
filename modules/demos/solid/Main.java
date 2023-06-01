79f0525a93eb9b68a29a4e285a15b6c93460a3f1
mrdbrg <rdg.brg@gmail.com>
2023-06-01 08:11:12 -0500

    add SOLID descriptions & implementation

--- modules/demos/solid/Main.java
+++ modules/demos/solid/Main.java
@@ -11,5 +11,19 @@ public class Main {
         List<Object> shapes = List.of(circle, square);
         int sum = areaCalculator.sum(shapes);
         System.out.println("Sum = " + sum);
+
+        /*
+            SOLID Principles
+            - Single Responsability
+            Each class should have only one sole purpose, and not be filled with excessive
+            functionality.
+
+            - Open Closed
+            Classes should be open for extension and closed for modification.
+
+            In other words, you should not have to rewrite an existing class for implementing new
+            features. Extend classes by the use of interfaces.
+
+        */
     }
 }
