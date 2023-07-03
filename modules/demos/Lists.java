8ae904ed69ee0d4505a9d8e67251751ba15da26f
mrdbrg <rdg.brg@gmail.com>
2023-07-03 08:03:15 -0500

    Lists

---
+++ modules/demos/Lists.java
@@ -0,0 +1,13 @@
+package com.havefunwith.modules.demos;
+
+public class Lists {
+
+    /**
+     * create a list of color names using the List interface and the ArrayList implementation
+     * display the size
+     * check for the existance of a color that returns true and one that returns false
+     * print the whole list
+     * loop through the list (use: enhanced for loop, forEach and traditional for i loop)
+     */
+
+}
