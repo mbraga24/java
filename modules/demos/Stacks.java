45d606fffb16df51abbbbca651ba676150e72b3c
mrdbrg <rdg.brg@gmail.com>
2023-07-05 08:23:13 -0500

    Statcks

---
+++ modules/demos/Stacks.java
@@ -0,0 +1,18 @@
+package com.havefunwith.modules.demos;
+
+public class Stacks {
+
+    public static void main(String[] args) {
+        // LIFO - Last In First Out
+
+        // create a stack
+        // push elements to the stack
+        // peek at the top element
+        // print size
+        // pop element from the top
+        // print new size
+        // check if stack is empty
+
+    }
+
+}
