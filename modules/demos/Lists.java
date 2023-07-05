613898a7bcafca597b1193b5e7bedf57be41921d
mrdbrg <rdg.brg@gmail.com>
2023-07-05 08:22:46 -0500

    Lists

--- modules/demos/Lists.java
+++ modules/demos/Lists.java
@@ -1,13 +1,34 @@
 package com.havefunwith.modules.demos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lists {
 
-    /**
-     * create a list of color names using the List interface and the ArrayList implementation
-     * display the size
-     * check for the existance of a color that returns true and one that returns false
-     * print the whole list
-     * loop through the list (use: enhanced for loop, forEach and traditional for i loop)
-     */
+    public static void main(String[] args) {
+
+        List<String> colors = new ArrayList<>();
+
+        colors.add("blue");
+        colors.add("red");
+        colors.add("yellow");
+        System.out.println("Colors size: " + colors.size());
+        System.out.println("Is there blue? " + colors.contains("blue"));
+        System.out.println("Is there green? " + colors.contains("green"));
+        System.out.println(colors);
+
+        System.out.println("=== Enhanced Loop ===");
+        for (String color : colors) {
+            System.out.println(color);
+        }
+        System.out.println("=== ForEach Loop ===");
+        colors.forEach(color -> {
+            System.out.println(color);
+        });
+        System.out.println("=== Traditional For Loop ===");
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
+        }
+    }
 
 }
