88dd33407b4d683923009101dddd4f441133f7e9
mrdbrg <rdg.brg@gmail.com>
2023-07-05 08:22:59 -0500

    Queues

---
+++ modules/demos/Queues.java
@@ -0,0 +1,12 @@
+package com.havefunwith.modules.demos;
+
+public class Queues {
+
+    // create a queue
+    // create a person class
+    // add 3 people to the queue
+    // check size
+    // peek the top of the queue
+    // remove the first item of the queue (poll)
+
+}
