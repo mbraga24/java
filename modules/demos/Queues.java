fac29221a6b23c70d4991e91603c092228efb122
mrdbrg <rdg.brg@gmail.com>
2023-07-06 07:25:51 -0500

    Queues

--- modules/demos/Queues.java
+++ modules/demos/Queues.java
@@ -1,12 +1,34 @@
 package com.havefunwith.modules.demos;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Queues {
 
-    // create a queue
-    // create a person class
-    // add 3 people to the queue
-    // check size
-    // peek the top of the queue
-    // remove the first item of the queue (poll)
+    public static void main(String[] args) {
+        Queue<Person> queue = new LinkedList<>();
+        queue.add(new Person("Marcelo"));
+        queue.add(new Person("Danyel"));
+        queue.add(new Person("João"));
 
+        System.out.println("Queue size: " + queue.size());
+        System.out.println("Queue peek: " + queue.peek().toString());
+        System.out.println("Queue poll: " + queue.poll().toString());
+        System.out.println("Queue size: " + queue.size());
+
+    }
+
+    private static class Person {
+        private String name;
+        Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
 }
