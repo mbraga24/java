84b736df78a8da17dd87a474b8e1ea4f9fe5eac1
mrdbrg <rdg.brg@gmail.com>
2023-07-06 07:26:07 -0500

    Stacks

--- modules/demos/Stacks.java
+++ modules/demos/Stacks.java
@@ -1,17 +1,21 @@
 package com.havefunwith.modules.demos;
 
+import java.util.Stack;
+
 public class Stacks {
 
     public static void main(String[] args) {
         // LIFO - Last In First Out
 
-        // create a stack
-        // push elements to the stack
-        // peek at the top element
-        // print size
-        // pop element from the top
-        // print new size
-        // check if stack is empty
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(33);
+        stack.push(44);
+        System.out.println("Stack size: " + stack.size());
+        System.out.println("Stack peek: " + stack.peek());
+        System.out.println("Stack pop: " + stack.pop());
+        System.out.println("Stack size: " + stack.size());
+        System.out.println("Stack empty: " + stack.empty());
 
     }
 
