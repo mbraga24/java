ee3cc413a2d9046c0ef2bca765853c09ef55891f
mrdbrg <rdg.brg@gmail.com>
2023-07-07 08:20:15 -0500

    Sets

---
+++ modules/Sets.java
@@ -0,0 +1,60 @@
+package com.havefunwith.modules;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Sets {
+
+    public static void main(String[] args) {
+
+        Set<Color> colors = new HashSet<>();
+
+        colors.add(new Color("blue"));
+        colors.add(new Color("blue"));
+        colors.add(new Color("red"));
+        colors.add(new Color("yellow"));
+
+        System.out.println("Colors size: " + colors.size());
+
+    }
+
+    private static class Color {
+        private String name;
+
+        Color(String name) {
+            this.name = name;
+        }
+
+        /**
+         * hashCode needs to be overriden in order to work with
+         * equals.
+         *
+         * @return
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        /**
+         * equals needs to be overriden in order to properly maintain
+         * unique elements in the set.
+         * @param obj
+         * @return
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Color color = (Color) obj;
+            return Objects.equals(name, color.name);
+        }
+
+//        @Override
+//        public boolean equals(Object obj) {
+//            return super.equals(obj);
+//        }
+    }
+
+}
