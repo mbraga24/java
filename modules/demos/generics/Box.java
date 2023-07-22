33472f84171638987f994f42f72164eeea8ae37b
mrdbrg <rdg.brg@gmail.com>
2023-07-22 15:03:24 -0500

    Generics

--- modules/demos/generics/Box.java
+++ modules/demos/generics/Box.java
@@ -12,4 +12,17 @@ public class Box<T> {
         return this.t;
     }
 
+    public <U extends Number> void inspect(U u){
+        System.out.println("T: " + t.getClass().getName());
+        System.out.println("U: " + u.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+        integerBox.set(new Integer(10));
+        integerBox.inspect("some text"); // error: this is still String!
+    }
+
+//    https://docs.oracle.com/javase/tutorial/java/generics/bounded.html#:~:text=There%20may%20be%20times%20when,bounded%20type%20parameters%20are%20for.
+
 }
