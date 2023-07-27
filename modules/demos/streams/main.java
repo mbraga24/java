fcbe4f7dead9066c929f32f5ee0c6c6182736ad0
mrdbrg <rdg.brg@gmail.com>
2023-07-27 08:46:35 -0500

    streams

--- modules/demos/streams/main.java
+++ modules/demos/streams/main.java
@@ -1,7 +1,57 @@
-package com.havefunwith.modules.demos;
+package com.havefunwith.modules.demos.streams;
 
-public class streams {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    
+public class main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("=========================");
+        System.out.println("IMPERATIVE APPROACH");
+        System.out.println("=========================");
+        System.out.println(imperativeApproach(MockData.getPeople()));
+        System.out.println("Size():: " + imperativeApproach(MockData.getPeople()).size());
+
+        System.out.println("=========================");
+        System.out.println("DECLARATIVE APPROACH");
+        System.out.println("=========================");
+        System.out.println(declarativeApproach(MockData.getPeople()));
+        System.out.println("Size():: " + declarativeApproach(MockData.getPeople()).size());
+
+    }
+
+    /*
+        1. Find people age less or equal 18
+        2. Then change implementation to find first 10 people
+     */
+
+    // IMPERATIVE
+    public static List<Person> imperativeApproach(List<Person> people) {
+        List<Person> youngPeople = new ArrayList<>();
+        int limit = 8;
+        for (Person p : people) {
+            if (p.getAge() <= 18 && limit > 0) {
+                youngPeople.add(p);
+                limit--;
+            }
+        }
+        return youngPeople;
+    }
+
+    /*
+        allMatch
+        anyMatch
+     */
+
+    public static List<Person> declarativeApproach(List<Person> people) {
+        return people.stream()
+                .filter(person -> person.getAge() <= 18) // intermediate operator
+                .limit(8) // intermediate operator
+                .collect(Collectors.toList()); // terminal operator
+    }
+
 
 }
